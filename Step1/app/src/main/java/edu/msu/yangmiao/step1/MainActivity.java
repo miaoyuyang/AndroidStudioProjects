57ae4315d20f043054a6066baaeb935fa3dce266
miaoyuyang <53418137+miaoyuyang@users.noreply.github.com>
2022-06-03 15:20:05 -0400

    Add files via upload

---
+++ Step1/app/src/main/java/edu/msu/yangmiao/step1/MainActivity.java
@@ -0,0 +1,18 @@
+package edu.msu.yangmiao.step1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+}
